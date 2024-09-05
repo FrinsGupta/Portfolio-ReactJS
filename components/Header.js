b1c822b3748675a684d574076cc0f7bef29e1198
FrinsGupta <pryncegupta@gmail.com>
2024-09-05 21:51:56 +0530

    Fixed Logo Position

--- components/Header.js
+++ components/Header.js
@@ -11,20 +11,45 @@ const Header = () => {
   return (
     <header className="absolute z-50 w-full h-16  flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-around items-center xl:gap-y-6 mt-48 xl:py-8 ">
-          {/* logo */}
+        {/* <div className="flex flex-col lg:flex-row justify-around items-center xl:gap-y-6  xl:py-8 "> */}
+        <div className="flex relative flex-col lg:flex-row justify-around items-center xl:gap-y-6   ">
+        {/* <div> */}
+          {/* socials */}
+          {/* <Socials />
+          </div> */}
+           {/* logo  */}
+          {/* <div className=" absolute left-60 top-24">
           <Link href={"/"}>
             <Image
               src={"/priilogo.png"}
-              width={220*2}
-              height={48*2}
+              width={220*1.5}
+              height={48*1.5}
               alt="full name/Logo svg"
               priority={true}
             />
           </Link> 
-          {/* socials */}
+          </div> */}
+          
+           {/* socials  */}
+          
+        
+            {/* logo  */}
+          <div className="absolute left-0 top-10 ">
+
+          <Link href={"/"}>
+            <Image
+              src={"/priilogo.png"}
+              width={220*1.3}
+              height={48*1.3}
+              alt="full name/Logo svg"
+              priority={true}
+            />
+          </Link> 
+          </div>
+          {/* <div className="absolute right-20"> */}
           <Socials />
-        </div>
+          {/* </div> */}
+          </div>
       </div>
     </header>
   );
